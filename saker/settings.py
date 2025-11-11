6589a53bef82306ed6ab07210f70a702b0d1feb6
mahmoud ahmed <mahmoud.ahmed.020663415145@gmail.com>
2025-11-11 21:18:46 +0200

    befor deploy**********

--- saker/settings.py
+++ saker/settings.py
@@ -55,10 +55,12 @@ INSTALLED_APPS = [
     'finance',
     'ai_agents',
     'ai_document',
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -213,3 +215,11 @@ SIMPLE_JWT = {
 }
 
 AUTH_USER_MODEL = 'api.Users'
+
+CORS_ALLOWED_ORIGINS = [
+    #"https://test.yourdomain.com",
+    "http://localhost:5173",
+]
+
+
+CORS_ALLOW_CREDENTIALS = True
