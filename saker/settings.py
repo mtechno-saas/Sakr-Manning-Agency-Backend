c339e04e385ca12a9d4b95680f4959bff41fba3b
mahmoud ahmed <mahmoud.ahmed.020663415145@gmail.com>
2025-12-16 18:02:46 +0200

    authy***********

--- saker/settings.py
+++ saker/settings.py
@@ -221,18 +221,21 @@ SIMPLE_JWT = {
 
 AUTH_USER_MODEL = 'api.Users'
 
-CORS_ALLOWED_ORIGINS = [
-    "https://sakr-maritime.vercel.app",
-    "https://test.sakrshipping.com",
-    "http://localhost:5173",
-    "http://api.backend.hs.vc",
-    "https://api.backend.hs.vc",
-]
+#CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "https://sakr-maritime.vercel.app",
+#     "https://test.sakrshipping.com",
+#     "http://localhost:5173",
+#     "http://api.backend.hs.vc",
+#     "https://api.backend.hs.vc",
+# ]
 
 
 CSRF_TRUSTED_ORIGINS = [
     "https://sakr-maritime.vercel.app",
     "https://test.sakrshipping.com",
+    "http://localhost:5173",
     "http://api.backend.hs.vc",
     "https://api.backend.hs.vc",
 ]
