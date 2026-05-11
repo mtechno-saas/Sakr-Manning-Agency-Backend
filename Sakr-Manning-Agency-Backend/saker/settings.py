1de5e4a0aed949f52cf44ccae64011e4d728b5dc
storm <storm@local>
2026-05-12 00:39:18 +0300

    Implement CustomPageNumberPagination to include current_page and total_pages in API response

--- Sakr-Manning-Agency-Backend/saker/settings.py
+++ Sakr-Manning-Agency-Backend/saker/settings.py
@@ -170,7 +170,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
         # Add pagination configuration
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPageNumberPagination',
     'PAGE_SIZE': 50,  # Adjust based on your needs
 }
 # --- Add this new configuration block for Simple JWT ---
